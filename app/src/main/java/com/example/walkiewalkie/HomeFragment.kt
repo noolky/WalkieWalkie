5c56b82f3e2e801092d1bed782ba2d110ab16367
ThiamYew <adenloo5201314@gmail.com>
2023-05-17 03:03:26 +0800

    First Home Commit

--- app/src/main/java/com/example/walkiewalkie/HomeFragment.kt
+++ app/src/main/java/com/example/walkiewalkie/HomeFragment.kt
@@ -6,12 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 
 
 class HomeFragment : Fragment() {
 
     lateinit var Name: TextView
+    private lateinit var buttonToStepCount: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -19,13 +21,15 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        Name = view.findViewById(R.id.NameOnHome) // Replace `textViewName` with the actual ID of your TextView
+        Name = view.findViewById(R.id.textView) // Replace `textViewName` with the actual ID of your TextView
 
         val sharedPreferences = context?.getSharedPreferences("your_preference_name", Context.MODE_PRIVATE)
         val username = sharedPreferences?.getString("username", null)
         Name.text = username
 
         return view
+
+
     }
 
 
